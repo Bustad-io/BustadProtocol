9d06b29e14708cd3f67534fee5d3043628cc151c
Rina Andriana <rina@andriana.no>
2022-04-06 08:14:50 +0200

    Fix Property.sol

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -3,38 +3,38 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Property is Ownable {
-    enum BustadPropertyStatus {
+    enum PropertyStatus {
         Active,
         Sold
     }
 
-    struct BustadProperty {
+    struct PurchasedProperty {
         bytes32 Id;
-        string cadastralNumber;
+        bytes32 cadastralNumber;
         string purchaseDate;
         uint256 estimatedValue;
         uint256 purchaseAmount;
         uint256 soldAmount;
         uint8 shareOfPropertyInPct;
-        BustadPropertyStatus status;
+        PropertyStatus status;
     }
 
-    mapping(bytes32 => BustadProperty) public propertyList;
+    mapping(bytes32 => PurchasedProperty) public propertyList;
 
     uint256 numberOfActiveProperties;
     uint256 numberOfSoldProperties;    
 
-    event PropertyPurchased(bytes32 id, string cadastralNumber, uint256 estimatedValue, string purchaseDate, uint256 purchaseAmount, uint8 shareOfPropertyInPct);
+    event PropertyPurchased(bytes32 id, bytes32 cadastralNumber, uint256 estimatedValue, string purchaseDate, uint256 purchaseAmount, uint8 shareOfPropertyInPct);
     event PropertySold(bytes32 id, uint256 soldAmount);
 
     constructor() {}
 
-    function getProperty(bytes32 id) external view returns (BustadProperty memory) {
+    function getProperty(bytes32 id) external view returns (PurchasedProperty memory) {
         return propertyList[id];        
     }
 
-    function addActiveProperty(bytes32 id, BustadProperty memory _prop) external onlyOwner {
-        require(_prop.status == BustadPropertyStatus.Active, "Property must have active status");
+    function addActiveProperty(bytes32 id, PurchasedProperty memory _prop) external onlyOwner {
+        require(_prop.status == PropertyStatus.Active, "Property must have active status");
         require(_prop.estimatedValue > 0, "invalid estimatedValue");
         require(_prop.purchaseAmount > 0, "invalid purchaseAmount");
         require(_prop.shareOfPropertyInPct > 0, "invalid shareOfPropertyInPct");
@@ -48,11 +48,11 @@ contract Property is Ownable {
     }
 
     function setPropertyAsSold(bytes32 id, uint256 amount) external onlyOwner {
-        BustadProperty storage property = propertyList[id];
+        PurchasedProperty storage property = propertyList[id];
 
         require(property.Id != 0, "Invalid property id");
 
-        property.status = BustadPropertyStatus.Sold;
+        property.status = PropertyStatus.Sold;
         property.soldAmount = amount;
 
         numberOfSoldProperties += 1;
@@ -61,6 +61,6 @@ contract Property is Ownable {
     }
 
     function removeProperty(bytes32 id) external onlyOwner {
-        propertyList[id] = BustadProperty(0, "","", 0,0,0,0,BustadPropertyStatus.Active);
+        propertyList[id] = PurchasedProperty(0, "","", 0,0,0,0,PropertyStatus.Active);
     }
 }
