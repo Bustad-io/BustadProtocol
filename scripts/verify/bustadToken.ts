026ad79fa6253116be90de833f461bfe60016b32
Rina Andriana <rina@andriana.no>
2022-06-27 10:13:33 +0200

    New deployment and verification scripts

---
+++ scripts/verify/bustadToken.ts
@@ -0,0 +1,18 @@
+import BustadTokenJSON from '../../deployments/rinkeby/BustadToken.json';
+const hre = require("hardhat");
+
+export async function main() {
+    const bustadTokenAddress = BustadTokenJSON.address;
+    const bustadTokeConstructorArguments = BustadTokenJSON.args;
+
+    await hre.run("verify:verify", {
+        address: bustadTokenAddress,
+        constructorArguments: bustadTokeConstructorArguments
+      });  
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
